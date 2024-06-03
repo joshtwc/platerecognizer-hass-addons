b960e476f9fa8c05f610cfae5b8b8775f5e86c3f
Josh Wood <josh@thewateringcan.ca>
2024-06-03 14:01:07 -0400

    test

--- platerecognizer_stream/entrypoint.sh
+++ platerecognizer_stream/entrypoint.sh
@@ -9,5 +9,7 @@ LICENSE_KEY=$(python3 -c "import json; print(json.load(open('$CONFIG_PATH'))['LI
 export TOKEN=$TOKEN
 export LICENSE_KEY=$LICENSE_KEY
 
+ln -s /share /user-data
+
 # Now run your application, passing the environment variable
 exec ./start.sh "$@"
