a22ee0282106ff5c6cfea3fcb3a9a056ad6d2ec0
Josh Wood <josh@thewateringcan.ca>
2024-06-03 12:49:18 -0400

    test

---
+++ platerecognizer_stream/pre_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONFIG_PATH=/data/options.json
+
+# Load options from the JSON file
+TOKEN=$(python3 -c "import json; print(json.load(open('$CONFIG_PATH'))['token'])")
+LICENCE_KEY=$(python3 -c "import json; print(json.load(open('$CONFIG_PATH'))['licence_key'])")
+
+# Export the environment variable
+export TOKEN=$TOKEN
+export LICENCE_KEY=$LICENCE_KEY
+
+# Now run your application, passing the environment variable
+bash run.sh
