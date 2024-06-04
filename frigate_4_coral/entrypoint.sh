fb62c4aa18b6ee8ef32c610ce6a638293755c6bc
Josh Wood <josh@thewateringcan.ca>
2024-06-04 10:19:47 -0400

    Custom Frigate

---
+++ frigate_4_coral/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bashio
+
+CONFIG_PATH=/data/options.json
+
+TOKEN=$(python3 -c "import json; print(json.load(open('$CONFIG_PATH'))['TOKEN'])")
+LICENSE_KEY=$(python3 -c "import json; print(json.load(open('$CONFIG_PATH'))['LICENSE_KEY'])")
+
+# Export the environment variable
+export TOKEN=$TOKEN
+export LICENSE_KEY=$LICENSE_KEY
+
+# Now run your application, passing the environment variable
+exec ./start.sh "$@"
